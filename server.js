1db11b26be8a896fcd92639f1d3271e174d9bdb3
JasonJhuboo <32700429+JasonJhuboo@users.noreply.github.com>
2020-04-29 21:28:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var low = require('lowdb')
 //var FileSync = require('lowdb/adapters/FileSync')
 //var adapter = new FileSync('.data/balderdashdb.json')
 
-var LocalStorage = require('lowdb/adapters/LocalStorage')
-var adapter = new LocalStorage('db')
+var Memory = require('lowdb/adapters/Memory')
+var adapter = new Memory()
 var db = low(adapter)
 var app = express();
 
